0b93f4aaf220d0e4a6d249f66396cd929091e17d
longyy <786603430@qq.com>
2017-03-19 23:23:31 +0800

    second and third level menu logic

--- resources/views/admin/public/left-side.blade.php
+++ resources/views/admin/public/left-side.blade.php
@@ -17,58 +17,28 @@
             <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 493px;"><div class="slim-scroll" data-height="auto" data-disable-fade-out="true" data-distance="0" data-size="5px" data-color="#333333" style="overflow: hidden; width: auto; height: 493px;"> <!-- nav -->
                     <nav class="nav-primary hidden-xs">
                         <ul class="nav">
-                            <li class="active"> <a href="index.html" class="active"> <i class="fa fa-dashboard icon"> <b class="bg-danger"></b> </i> <span>Workset</span> </a> </li>
-                            <li> <a href="#layout"> <i class="fa fa-columns icon"> <b class="bg-warning"></b> </i> <span class="pull-right"> <i class="fa fa-angle-down text"></i> <i class="fa fa-angle-up text-active"></i> </span> <span>Layouts</span> </a>
-                                <ul class="nav lt">
-                                    <li> <a href="layout-c.html"> <i class="fa fa-angle-right"></i> <span>Color option</span> </a> </li>
-                                    <li> <a href="layout-r.html"> <i class="fa fa-angle-right"></i> <span>Right nav</span> </a> </li>
-                                    <li> <a href="layout-h.html"> <i class="fa fa-angle-right"></i> <span>H-Layout</span> </a> </li>
-                                </ul>
-                            </li>
-                            <li> <a href="#uikit"> <i class="fa fa-flask icon"> <b class="bg-success"></b> </i> <span class="pull-right"> <i class="fa fa-angle-down text"></i> <i class="fa fa-angle-up text-active"></i> </span> <span>UI kit</span> </a>
-                                <ul class="nav lt">
-                                    <li> <a href="buttons.html"> <i class="fa fa-angle-right"></i> <span>Buttons</span> </a> </li>
-                                    <li> <a href="icons.html"> <b class="badge bg-info pull-right">369</b> <i class="fa fa-angle-right"></i> <span>Icons</span> </a> </li>
-                                    <li> <a href="grid.html"> <i class="fa fa-angle-right"></i> <span>Grid</span> </a> </li>
-                                    <li> <a href="widgets.html"> <b class="badge pull-right">8</b> <i class="fa fa-angle-right"></i> <span>Widgets</span> </a> </li>
-                                    <li> <a href="components.html"> <i class="fa fa-angle-right"></i> <span>Components</span> </a> </li>
-                                    <li> <a href="list.html"> <i class="fa fa-angle-right"></i> <span>List group</span> </a> </li>
-                                    <li> <a href="#table"> <i class="fa fa-angle-down text"></i> <i class="fa fa-angle-up text-active"></i> <span>Table</span> </a>
-                                        <ul class="nav bg">
-                                            <li> <a href="table-static.html"> <i class="fa fa-angle-right"></i> <span>Table static</span> </a> </li>
-                                            <li> <a href="table-datatable.html"> <i class="fa fa-angle-right"></i> <span>Datatable</span> </a> </li>
-                                            <li> <a href="table-datagrid.html"> <i class="fa fa-angle-right"></i> <span>Datagrid</span> </a> </li>
-                                        </ul>
+
+                            <!-- 二级和三级菜单 -->
+                            @foreach ($aPageMenu as $aVal)
+                                @if($aVal->iLevel == 1)
+                                    {{--<li class="active">--}}
+                                    <li>
+                                        <a href="#layout"> <i class="fa fa-columns icon"> <b class="bg-warning"></b> </i> <span class="pull-right"> <i class="fa fa-angle-down text"></i> <i class="fa fa-angle-up text-active"></i> </span> <span>{{$aVal->sName}}</span> </a>
+                                    @foreach ($aPageMenu as $aVVal)
+                                        @if($aVVal->iLevel == 2 && $aVVal->iParentID == $aVal->iAutoID)
+                                                <ul class="nav lt">
+                                                    <li> <a href="layout-c.html"> <i class="fa fa-angle-right"></i> <span>{{$aVVal->sName}}</span> </a> </li>
+                                                </ul>
+                                        @endif
+                                    @endforeach
+
                                     </li>
-                                    <li> <a href="#form"> <i class="fa fa-angle-down text"></i> <i class="fa fa-angle-up text-active"></i> <span>Form</span> </a>
-                                        <ul class="nav bg">
-                                            <li> <a href="form-elements.html"> <i class="fa fa-angle-right"></i> <span>Form elements</span> </a> </li>
-                                            <li> <a href="form-validation.html"> <i class="fa fa-angle-right"></i> <span>Form validation</span> </a> </li>
-                                            <li> <a href="form-wizard.html"> <i class="fa fa-angle-right"></i> <span>Form wizard</span> </a> </li>
-                                        </ul>
-                                    </li>
-                                    <li> <a href="chart.html"> <i class="fa fa-angle-right"></i> <span>Chart</span> </a> </li>
-                                    <li> <a href="fullcalendar.html"> <i class="fa fa-angle-right"></i> <span>Fullcalendar</span> </a> </li>
-                                    <li> <a href="portlet.html"> <i class="fa fa-angle-right"></i> <span>Portlet</span> </a> </li>
-                                    <li> <a href="timeline.html"> <i class="fa fa-angle-right"></i> <span>Timeline</span> </a> </li>
-                                </ul>
-                            </li>
-                            <li> <a href="#pages"> <i class="fa fa-file-text icon"> <b class="bg-primary"></b> </i> <span class="pull-right"> <i class="fa fa-angle-down text"></i> <i class="fa fa-angle-up text-active"></i> </span> <span>Pages</span> </a>
-                                <ul class="nav lt">
-                                    <li> <a href="gallery.html"> <i class="fa fa-angle-right"></i> <span>Gallery</span> </a> </li>
-                                    <li> <a href="profile.html"> <i class="fa fa-angle-right"></i> <span>Profile</span> </a> </li>
-                                    <li> <a href="invoice.html"> <i class="fa fa-angle-right"></i> <span>Invoice</span> </a> </li>
-                                    <li> <a href="intro.html"> <i class="fa fa-angle-right"></i> <span>Intro</span> </a> </li>
-                                    <li> <a href="master.html"> <i class="fa fa-angle-right"></i> <span>Master</span> </a> </li>
-                                    <li> <a href="gmap.html"> <i class="fa fa-angle-right"></i> <span>Google Map</span> </a> </li>
-                                    <li> <a href="signin.html"> <i class="fa fa-angle-right"></i> <span>Signin</span> </a> </li>
-                                    <li> <a href="signup.html"> <i class="fa fa-angle-right"></i> <span>Signup</span> </a> </li>
-                                    <li> <a href="404.html"> <i class="fa fa-angle-right"></i> <span>404</span> </a> </li>
-                                </ul>
-                            </li>
-                            <li> <a href="mail.html"> <b class="badge bg-danger pull-right">3</b> <i class="fa fa-envelope-o icon"> <b class="bg-primary dker"></b> </i> <span>Message</span> </a> </li>
-                            <li> <a href="notebook.html"> <i class="fa fa-pencil icon"> <b class="bg-info"></b> </i> <span>Notes</span> </a> </li>
+
+                                @endif
+                            @endforeach
+
                         </ul>
+
                     </nav>
                     <!-- / nav --> </div><div class="slimScrollBar" style="width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; z-index: 99; right: 0px; height: 493px; background: rgb(51, 51, 51);"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; opacity: 0.2; z-index: 90; right: 0px; background: rgb(51, 51, 51);"></div></div>
         </section>
